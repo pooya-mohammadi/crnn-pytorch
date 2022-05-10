0dbc6ea2cb56b9ad25a300a288840a655e75c576
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-05-11 00:33:33 +0430

    Fix crnn model with deep-utils

--- crnn.py
+++ crnn.py
@@ -19,19 +19,39 @@ class CRNN(nn.Module):
         block_5 = BlocksTorch.conv_norm_act(512, 512, pooling='max', norm="bn", pooling_s=(2, 1), pooling_k=(2, 1))
         block_6 = BlocksTorch.conv_norm_act(512, 512, k=2, p=0)
         self.cnn = nn.Sequential(block_0, block_1, block_2, block_3, block_4, block_5, block_6)
-        self.rnn = nn.Sequential(
-            nn.Linear(512 * (img_h // 16 - 1), lstm_input),
-            nn.LSTM(lstm_input, n_hidden, bidirectional=True),
-            nn.LSTM(2 * n_hidden, n_hidden, bidirectional=True)
-        )
+        self.cnn2rnn = nn.Linear(512 * (img_h // 16 - 1), lstm_input)
+
+        # RNN
+        self.rnn_01 = nn.LSTM(lstm_input, n_hidden, bidirectional=True)
+        self.rnn_02 = nn.LSTM(2 * n_hidden, n_hidden, bidirectional=True)
+
+        # Classifiers
         self.classifier = nn.Linear(2 * n_hidden, n_classes)
+
+    @staticmethod
+    def weights_init(m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            m.weight.data.normal_(0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            m.weight.data.normal_(1.0, 0.02)
+            m.bias.data.fill_(0)
+        elif classname.find('Linear') != -1:
+            torch.nn.init.xavier_uniform(m.weight)
+            m.bias.data.fill_(0.01)
 
     def forward(self, input):
         # conv features
         conv = self.cnn(input)
+        batch_size, channels, h, w = conv.shape
+        conv = conv.view((batch_size, channels * h, w)).permute(2, 0, 1)
+        rnn_input = self.cnn2rnn(conv)
         # rnn features
-        output = self.rnn(conv)
-        cls = self.classifier(output)
+        x, _ = self.rnn_01(rnn_input)
+        rnn_output, _ = self.rnn_02(x)
+
+        # Classifier
+        cls = self.classifier(rnn_output)
         # add log_softmax to converge output
         output = F.log_softmax(cls, dim=2)
         if self.return_cls:
@@ -41,8 +61,10 @@ class CRNN(nn.Module):
 
 
 if __name__ == '__main__':
+    import torch
+
     m = CRNN(32, 1, 100, 256)
     print(m)
-    # sample = torch.randn((1, 1, 32, 100))
-    # m(sample)
+    sample = torch.randn((1, 1, 32, 100))
+    m(sample)
     # summary(m, input_size=(1, 32, 100), batch_size=1, device='cpu')
