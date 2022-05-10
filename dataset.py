bbdfb5b576d7e199d806f7fc6d0a011e52feaaf5
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-05-10 18:56:04 +0430

    Fix dataset.py

--- dataset.py
+++ dataset.py
@@ -1,24 +1,66 @@
 import os
+from argparse import ArgumentParser
 from os.path import join
 from os.path import split
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
-import cv2
 from PIL import Image
-import params
-from deep_utils import split_extension
+from deep_utils import split_extension, log_print
+from tqdm import tqdm
+from alphabets import ALPHABETS
+from params import IMG_H, IMG_W
 
 
 class CRNNDataset(Dataset):
+    DEFAULT_TRANSFORM = transforms.Compose([
+        transforms.Grayscale(),
+        transforms.Resize((IMG_H, IMG_W)),
+        transforms.ToTensor()]
+    )
 
-    def __init__(self, root=None, transform=None, target_transform=None):
+    def __init__(self, root, characters, transform=None, logger=None):
+        self.transform = self.DEFAULT_TRANSFORM if transform is None else transform
+        # index zero is reserved for CTC's delimiter
+        self.char2label = {char: i + 1 for i, char in enumerate(characters)}
+        self.label2char = {label: char for char, label in self.char2label.items()}
+        self.image_paths, self.labels, self.labels_length = self.get_image_paths(root, characters,
+                                                                                 chars2label=self.char2label,
+                                                                                 logger=logger)
+        self.n_classes = len(self.label2char) + 1  # + 1 is representative of CTC's delimiter!
 
-        self.image_paths = [join(root, img_name) for img_name in os.listdir(root) if
-                            split_extension(img_name)[-1].lower() in ['.jpg', '.png', '.jpeg']]
+    @staticmethod
+    def text2label(char2label: dict, text: str):
+        return [char2label[t] for t in text]
 
-        self.transform = transform
-        self.target_transform = target_transform
+    @staticmethod
+    def get_image_paths(root, chars, chars2label, logger=None):
+        paths, labels, labels_length = [], [], []
+        discards = 0
+        for img_name in os.listdir(root):
+            img_path = join(root, img_name)
+            try:
+                if split_extension(img_name)[-1].lower() in ['.jpg', '.png', '.jpeg']:
+                    text = CRNNDataset.get_label(img_path)
+                    is_valid = CRNNDataset.check_validity(text, chars)
+                    if is_valid:
+                        label = CRNNDataset.text2label(chars2label, text)
+                        labels.append(label)
+                        paths.append(img_path)
+                        labels_length.append(len(label))
+                    else:
+                        log_print(logger, f"[Warning] text for sample: {img_path} is invalid. Skipping...")
+                        discards += 1
+                else:
+                    log_print(logger, f"[Warning] sample: {img_path} does not have a valid extension. Skipping...")
+                    discards += 1
+            except:
+                log_print(logger, f"[Warning] sample: {img_path} is not valid. Skipping...")
+                discards += 1
+        assert len(labels) == len(paths)
+        log_print(logger, f"Successfully gathered {len(labels)} samples and discarded {discards} samples!")
+
+        return paths, labels, labels_length
 
     def __len__(self):
         return len(self.image_paths)
@@ -26,26 +68,73 @@ class CRNNDataset(Dataset):
     def __getitem__(self, index):
         assert index <= len(self), 'index range error'
         img_path = self.image_paths[index]
-        if params.nc == 1:
-            img = Image.open(img_path).convert('L')
-        else:
-            img = Image.open(img_path).convert('RGB')
+        img = Image.open(img_path)
+        img = self.transform(img).unsqueeze(0)
 
-        if self.transform is not None:
-            img = self.transform(img)
-        label = self.get_label(img_path)
+        label = torch.LongTensor(self.labels[index]).unsqueeze(0)
+        label_length = torch.LongTensor(self.labels_length[index]).unsqueeze(0)
 
-        if self.target_transform is not None:
-            label = self.target_transform(label)
-
-        return img, label
+        return img, label, label_length
 
     @staticmethod
     def get_label(img_path):
         label = split_extension(split(img_path)[-1])[0]
-        label = label.split('_')[-1].split('-')[0]
+        label = label.split('_')[0]
         return label
 
+    @staticmethod
+    def check_validity(text, chars):
+        for c in text:
+            if c not in chars:
+                return False
+        return True
+
+
+def collate_fn(batch):
+    images, labels, labels_lengths = zip(*batch)
+    images = torch.cat(images, dim=0)
+    labels = torch.cat(labels, dim=0)
+    labels_lengths = torch.cat(labels_lengths, dim=0)
+    return images, labels, labels_lengths
+
+
+def get_mean_std(data_loader: DataLoader):
+    """
+    Getting channel wise mean and std
+    :param data_loader:
+    :return:
+    """
+    mean, std = 0, 0
+    n_samples = len(data_loader.dataset)
+    for images, labels, labels_lengths in tqdm(data_loader, desc="Getting mean and std"):
+        # channel wise
+        mean += torch.sum(torch.mean(images, dim=(2, 3)), dim=0)
+        std += torch.sum(torch.std(images, dim=(2, 3)), dim=0)
+    mean /= n_samples
+    std /= n_samples
+    return mean, std
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("--train_directory",
+                        default="/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/train")
+    parser.add_argument("--batch_size", default=128)
+    parser.add_argument("--alphabet_name", default="FA_LPR", help="alphabet name from alphabets.py module")
+    args = parser.parse_args()
+
+    transformations = transforms.Compose([
+        transforms.Grayscale(),
+        transforms.RandomHorizontalFlip(p=0.5),  # Makes no difference :D
+        transforms.Resize((IMG_H, IMG_W)),
+        transforms.ToTensor()]
+    )
+
+    dataset = CRNNDataset(root=args.train_directory, transform=transformations,
+                          characters=ALPHABETS[args.alphabet_name])
+    data_loader = DataLoader(dataset, batch_size=args.batch_size, collate_fn=collate_fn)
+    mean, std = get_mean_std(data_loader)
+    log_print(None, f"MEAN: {mean}, STD: {std}")
 
 #
 # def get_train_transform(random_resize_crop=dict(size=None, scale=(0.08, 1.0),
@@ -73,86 +162,86 @@ class CRNNDataset(Dataset):
 # get_train_transform()
 
 
-class ResizeNormalize:
-
-    def __init__(self, size, interpolation=Image.BILINEAR):
-        self.size = size
-        self.interpolation = interpolation
-        self.toTensor = transforms.ToTensor()
-
-    def __call__(self, img):
-        img = img.resize(self.size, self.interpolation)
-        img = self.toTensor(img)
-        img.sub_(0.5).div_(0.5)
-        return img
-
-
-class CV2ResizeNormalize(object):
-
-    def __init__(self, size, interpolation=cv2.INTER_LINEAR):
-        self.size = size
-        self.interpolation = interpolation
-        self.toTensor = transforms.ToTensor()
-
-    def __call__(self, img):
-        img = cv2.resize(img, self.size, interpolation=self.interpolation)
-        # img = img.resize(self.size, self.interpolation)
-        img = self.toTensor(img)
-        img.sub_(0.5).div_(0.5)
-        return img
-
-
-# class RandomSequentialSampler(sampler.Sampler):
+# class ResizeNormalize:
 #
-#     def __init__(self, data_source, batch_size):
-#         self.num_samples = len(data_source)
-#         self.batch_size = batch_size
+#     def __init__(self, size, interpolation=Image.BILINEAR):
+#         self.size = size
+#         self.interpolation = interpolation
+#         self.toTensor = transforms.ToTensor()
 #
-#     def __iter__(self):
-#         n_batch = len(self) // self.batch_size
-#         tail = len(self) % self.batch_size
-#         index = torch.LongTensor(len(self)).fill_(0)
-#         for i in range(n_batch):
-#             random_start = random.randint(0, len(self) - self.batch_size)
-#             batch_index = random_start + torch.range(0, self.batch_size - 1)
-#             index[i * self.batch_size:(i + 1) * self.batch_size] = batch_index
-#         # deal with tail
-#         if tail:
-#             random_start = random.randint(0, len(self) - self.batch_size)
-#             tail_index = random_start + torch.range(0, tail - 1)
-#             index[(i + 1) * self.batch_size:] = tail_index
+#     def __call__(self, img):
+#         img = img.resize(self.size, self.interpolation)
+#         img = self.toTensor(img)
+#         img.sub_(0.5).div_(0.5)
+#         return img
 #
-#         return iter(index)
 #
-#     def __len__(self):
-#         return self.num_samples
-
-
-class AlignCollate(object):
-
-    def __init__(self, imgH=32, imgW=100, keep_ratio=False, min_ratio=1):
-        self.imgH = imgH
-        self.imgW = imgW
-        self.keep_ratio = keep_ratio
-        self.min_ratio = min_ratio
-
-    def __call__(self, batch):
-        images, labels = zip(*batch)
-
-        imgH = self.imgH
-        imgW = self.imgW
-        # if self.keep_ratio:
-        #     ratios = []
-        #     for image in images:
-        #         w, h = image.size
-        #         ratios.append(w / float(h))
-        #     ratios.sort()
-        #     max_ratio = ratios[-1]
-        #     imgW = int(np.floor(max_ratio * imgH))
-        #     imgH = max(imgH * self.min_ratio, imgW)  # assure imgH >= imgW
-
-        transform = ResizeNormalize((imgW, imgH))
-        images = [transform(image) for image in images]
-        images = torch.cat([t.unsqueeze(0) for t in images], 0)
-
-        return images, labels
+# class CV2ResizeNormalize(object):
+#
+#     def __init__(self, size, interpolation=cv2.INTER_LINEAR):
+#         self.size = size
+#         self.interpolation = interpolation
+#         self.toTensor = transforms.ToTensor()
+#
+#     def __call__(self, img):
+#         img = cv2.resize(img, self.size, interpolation=self.interpolation)
+#         # img = img.resize(self.size, self.interpolation)
+#         img = self.toTensor(img)
+#         img.sub_(0.5).div_(0.5)
+#         return img
+#
+#
+# # class RandomSequentialSampler(sampler.Sampler):
+# #
+# #     def __init__(self, data_source, batch_size):
+# #         self.num_samples = len(data_source)
+# #         self.batch_size = batch_size
+# #
+# #     def __iter__(self):
+# #         n_batch = len(self) // self.batch_size
+# #         tail = len(self) % self.batch_size
+# #         index = torch.LongTensor(len(self)).fill_(0)
+# #         for i in range(n_batch):
+# #             random_start = random.randint(0, len(self) - self.batch_size)
+# #             batch_index = random_start + torch.range(0, self.batch_size - 1)
+# #             index[i * self.batch_size:(i + 1) * self.batch_size] = batch_index
+# #         # deal with tail
+# #         if tail:
+# #             random_start = random.randint(0, len(self) - self.batch_size)
+# #             tail_index = random_start + torch.range(0, tail - 1)
+# #             index[(i + 1) * self.batch_size:] = tail_index
+# #
+# #         return iter(index)
+# #
+# #     def __len__(self):
+# #         return self.num_samples
+#
+#
+# class AlignCollate(object):
+#
+#     def __init__(self, imgH=32, imgW=100, keep_ratio=False, min_ratio=1):
+#         self.imgH = imgH
+#         self.imgW = imgW
+#         self.keep_ratio = keep_ratio
+#         self.min_ratio = min_ratio
+#
+#     def __call__(self, batch):
+#         images, labels = zip(*batch)
+#
+#         imgH = self.imgH
+#         imgW = self.imgW
+#         # if self.keep_ratio:
+#         #     ratios = []
+#         #     for image in images:
+#         #         w, h = image.size
+#         #         ratios.append(w / float(h))
+#         #     ratios.sort()
+#         #     max_ratio = ratios[-1]
+#         #     imgW = int(np.floor(max_ratio * imgH))
+#         #     imgH = max(imgH * self.min_ratio, imgW)  # assure imgH >= imgW
+#
+#         transform = ResizeNormalize((imgW, imgH))
+#         images = [transform(image) for image in images]
+#         images = torch.cat([t.unsqueeze(0) for t in images], 0)
+#
+#         return images, labels
