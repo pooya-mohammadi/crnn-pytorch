Fix normalization and flip

inference.py
@@ -3,29 +3,27 @@ from pathlib import Path
 from typing import Union
 import numpy as np
 import torch
-import torchvision.transforms as T
+import torchvision.transforms as transforms
 from PIL import Image, ImageDraw, ImageFont
-from alphabets import ALPHABETS
 from train import LitCRNN
 from deep_utils import CTCDecoder, show_destroy_cv2
 import time
 
 
 class CRNNPred:
-    def __init__(self, model_path, characters, img_height=32, img_width=100,
-                 decode_method='greedy'):
-        self.char2label = {char: i + 1 for i, char in enumerate(characters)}
-        self.label2char = {label: char for char, label in self.char2label.items()}
+    def __init__(self, model_path, decode_method='greedy'):
         self.decode_method = decode_method
         self.model = LitCRNN.load_from_checkpoint(model_path)
         self.model.eval()
-
-        self.transformer = T.Compose([
-            T.Grayscale(),
-            T.RandomHorizontalFlip(p=1),
-            T.Resize((img_height, img_width)),
-            T.ToTensor()]
+        state_dict = torch.load(model_path)
+        self.label2char = state_dict['label2char']
+        self.transformer = transforms.Compose([
+            transforms.Grayscale(),
+            transforms.Resize((state_dict['img_height'], state_dict['img_width'])),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=state_dict['mean'], std=state_dict['std'])]
         )
+        del state_dict
 
     def detect(self, img: Union[str, Path, np.ndarray]):
         if isinstance(img, np.ndarray):
@@ -43,10 +41,9 @@ class CRNNPred:
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--model_path", default="output/exp_1/best.ckpt")
-    parser.add_argument("--alphabet_name", default="FA_LPR", help="alphabet name from alphabets.py module")
-    parser.add_argument("--img_path", default="sample_images/۱۳ج۷۷۲۴۴_9779.jpg")
+    parser.add_argument("--img_path", default="sample_images/image_02.jpg")
     args = parser.parse_args()
-    model = CRNNPred(args.model_path, characters=ALPHABETS[args.alphabet_name])
+    model = CRNNPred(args.model_path)
     img = Image.open(args.img_path)
     tic = time.time()
     prediction = model.detect(args.img_path)

settings.py
@@ -26,17 +26,16 @@ class Config(BasicConfig):
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
     LR = 0.0005
     LR_PATIENCE = 10
-    LR_REDUCE_FACTOR = 0.5
-    SEAD = 1234
+    LR_REDUCE_FACTOR = 0.1
     BATCH_SIZE = 128
-    EPOCHS = 100
+    EPOCHS = 200
     N_WORKERS = 8
 
     TRANSFORMATION = transforms.Compose([
         transforms.Grayscale(),
-        transforms.RandomHorizontalFlip(p=1),  # Makes no difference :D
         transforms.Resize((BasicConfig.IMG_H, BasicConfig.IMG_W)),
-        transforms.ToTensor()]
+        transforms.ToTensor(),
+        transforms.Normalize(mean=BasicConfig.MEAN, std=BasicConfig.STD), ]
     )
 
     ALPHABETS = ALPHABETS[BasicConfig.ALPHABET_NAME]
@@ -44,7 +43,7 @@ class Config(BasicConfig):
     LABEL2CHAR = dict()
 
     # Early stopping
-    EARLY_STOPPING_PATIENCE = 20
+    EARLY_STOPPING_PATIENCE = 30
 
 
 Config.CHAR2LABEL = {char: i + 1 for i, char in enumerate(Config.ALPHABETS)}

train.py
@@ -1,6 +1,7 @@
+from os.path import join
 import torch
 import pytorch_lightning as pl
-from deep_utils import CTCDecoder, mkdir_incremental
+from deep_utils import mkdir_incremental
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from dataset import CRNNDataset
 from crnn import CRNN
@@ -12,10 +13,8 @@ torch.backends.cudnn.benchmark = True
 
 
 class LitCRNN(pl.LightningModule):
-    def __init__(self, label2char=None, decode_method="greedy"):
+    def __init__(self):
         super(LitCRNN, self).__init__()
-        self.label2char = label2char
-        self.decode_method = decode_method
         self.model = CRNN(img_h=Config.IMG_H,
                           n_channels=Config.N_CHANNELS,
                           n_classes=Config.N_CLASSES,
@@ -115,12 +114,25 @@ def main():
                          min_epochs=Config.EPOCHS // 10,
                          callbacks=[early_stopping, model_checkpoint, learning_rate_monitor],
                          default_root_dir=output_dir)
-    lit_crnn = LitCRNN(Config.LABEL2CHAR)
+    lit_crnn = LitCRNN()
     train_loader, val_loader = lit_crnn.get_loaders()
     trainer.fit(model=lit_crnn, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
     trainer.test(lit_crnn, ckpt_path="best", dataloaders=val_loader)
     trainer.test(lit_crnn, ckpt_path="best", dataloaders=train_loader)
+
+    # Adding artifacts to weights
+    weight_path = join(output_dir, f"{Config.FILE_NAME}.ckpt")
+    best_weight = torch.load(weight_path)
+    best_weight['img_height'] = Config.IMG_H
+    best_weight['img_width'] = Config.IMG_W
+    best_weight['n_channels'] = Config.N_CHANNELS
+    best_weight['n_hidden'] = Config.N_HIDDEN
+    best_weight['mean'] = Config.MEAN
+    best_weight['std'] = Config.STD
+    best_weight['label2char'] = Config.LABEL2CHAR
+    best_weight['n_classes'] = Config.N_CLASSES
+    torch.save(best_weight, weight_path)
 
 
 if __name__ == '__main__':