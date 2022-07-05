11b39361f1808e68e28f617dfb05d5ba5d20fbb3
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2022-07-05 20:39:44 +0430

    Add argument parser and convert Config to config

--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
 import torch
 from torchvision import transforms
-
+from dataclasses import dataclass
 from alphabets import ALPHABETS
 
 
+@dataclass(init=True)
 class BasicConfig:
     IMG_H = 32  # the height of the input image to network
     IMG_W = 100  # the width of the input image to network
@@ -12,39 +13,63 @@ class BasicConfig:
 
     # Modify
     N_CLASSES = 35
-    MEAN = [0.4845]
-    STD = [0.1884]
+    mean = [0.4845]
+    std = [0.1884]
     ALPHABET_NAME = "FA_LPR"
-    TRAIN_ROOT = '/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/train'
-    VAL_ROOT = '/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/val'
-    OUTPUT_DIR = "output"
+    train_directory = '/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/train'
+    val_directory = '/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/val'
+    output_dir = "output"
 
 
+@dataclass(init=True)
 class Config(BasicConfig):
     N_HIDDEN = 256  # size of the lstm hidden state
     N_CHANNELS = 1
-    DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     LR = 0.0005
     LR_PATIENCE = 10
     LR_REDUCE_FACTOR = 0.1
     BATCH_SIZE = 128
-    EPOCHS = 200
+    epochs = 200
     N_WORKERS = 8
 
     TRANSFORMATION = transforms.Compose([
         transforms.Grayscale(),
         transforms.Resize((BasicConfig.IMG_H, BasicConfig.IMG_W)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=BasicConfig.MEAN, std=BasicConfig.STD), ]
+        transforms.Normalize(mean=BasicConfig.mean, std=BasicConfig.std), ]
     )
 
-    ALPHABETS = ALPHABETS[BasicConfig.ALPHABET_NAME]
+    alphabets = ALPHABETS[BasicConfig.ALPHABET_NAME]
     CHAR2LABEL = dict()
     LABEL2CHAR = dict()
 
     # Early stopping
     EARLY_STOPPING_PATIENCE = 30
 
+    def update_config_param(self, args):
+        variables = vars(args)
+        for k, v in variables.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
+            else:
+                raise ValueError(f"value {k} is not defined in Config...")
+        self.update()
 
-Config.CHAR2LABEL = {char: i + 1 for i, char in enumerate(Config.ALPHABETS)}
-Config.LABEL2CHAR = {label: char for char, label in Config.CHAR2LABEL.items()}
+    def update(self):
+        self.CHAR2LABEL = {char: i + 1 for i, char in enumerate(Config.alphabets)}
+        self.LABEL2CHAR = {label: char for char, label in self.CHAR2LABEL.items()}
+
+    def __repr__(self):
+        variables = vars(self)
+        return f"{self.__class__.__name__} -> " + ", ".join(f"{k}: {v}" for k, v in variables.items())
+
+    def vars(self) -> dict:
+        out = dict()
+        for key in dir(self):
+            val = getattr(self, key)
+            if (key.startswith("__") and key.endswith("__")) or type(val).__name__ == "method":
+                continue
+            else:
+                out[key] = val
+        return out
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 from os.path import join
+from argparse import ArgumentParser
+from pathlib import Path
 import torch
 import pytorch_lightning as pl
 from deep_utils import mkdir_incremental
@@ -13,12 +15,13 @@ torch.backends.cudnn.benchmark = True
 
 
 class LitCRNN(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, img_h, n_channels, n_classes, n_hidden, lr, lr_reduce_factor, lr_patience):
         super(LitCRNN, self).__init__()
-        self.model = CRNN(img_h=Config.IMG_H,
-                          n_channels=Config.N_CHANNELS,
-                          n_classes=Config.N_CLASSES,
-                          n_hidden=Config.N_HIDDEN)
+        self.save_hyperparameters()
+        self.model = CRNN(img_h=self.hparams.img_h,
+                          n_channels=self.hparams.n_channels,
+                          n_classes=self.hparams.n_classes,
+                          n_hidden=self.hparams.n_hidden)
         self.model.apply(self.model.weights_init)
         self.criterion = CTCLoss(reduction='sum')
 
@@ -34,13 +37,6 @@ class LitCRNN(pl.LightningModule):
         logits = self.model(images)
         input_lengths = torch.LongTensor([logits.size(0)] * batch_size)
         loss = self.criterion(logits, labels, input_lengths, labels_lengths)
-        # if train:
-        #     acc = 0
-        # else:
-        #     preds = torch.transpose(logits, 1, 0).detach().cpu().numpy()
-        #     preds = CTCDecoder.ctc_decode(preds, decoder_name=self.decode_method, label2char=None)
-        #     acc = sum("-".join([str(p) for p in pred]) == "-".join([str(l) for l in labels]) for pred, label in
-        #               zip(preds, labels.detach().cpu().numpy()))
         return loss, batch_size
 
     @staticmethod
@@ -77,61 +73,82 @@ class LitCRNN(pl.LightningModule):
         return {"loss": loss, "bs": bs}
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=Config.LR)
-        scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=Config.LR_REDUCE_FACTOR,
-                                      patience=Config.LR_PATIENCE, verbose=True)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+        scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=self.hparams.lr_reduce_factor,
+                                      patience=self.hparams.lr_patience, verbose=True)
         return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
 
     @staticmethod
-    def get_loaders():
-        train_dataset = CRNNDataset(root=Config.TRAIN_ROOT, characters=Config.ALPHABETS,
-                                    transform=Config.TRANSFORMATION)
+    def get_loaders(config):
+        train_dataset = CRNNDataset(root=config.train_directory, characters=config.alphabets,
+                                    transform=config.TRANSFORMATION)
         train_loader = torch.utils.data.DataLoader(train_dataset,
-                                                   batch_size=Config.BATCH_SIZE,
+                                                   batch_size=config.BATCH_SIZE,
                                                    shuffle=True,
-                                                   num_workers=Config.N_WORKERS,
+                                                   num_workers=config.N_WORKERS,
                                                    collate_fn=train_dataset.collate_fn
                                                    )
 
-        val_dataset = CRNNDataset(root=Config.VAL_ROOT, characters=Config.ALPHABETS, transform=Config.TRANSFORMATION)
+        val_dataset = CRNNDataset(root=config.val_directory, characters=config.alphabets,
+                                  transform=config.TRANSFORMATION)
         val_loader = torch.utils.data.DataLoader(val_dataset,
                                                  shuffle=True,
-                                                 batch_size=Config.BATCH_SIZE,
-                                                 num_workers=Config.N_WORKERS,
+                                                 batch_size=config.BATCH_SIZE,
+                                                 num_workers=config.N_WORKERS,
                                                  collate_fn=val_dataset.collate_fn)
 
         return train_loader, val_loader
 
 
 def main():
-    output_dir = mkdir_incremental(Config.OUTPUT_DIR)
-    early_stopping = EarlyStopping(monitor='val_loss', patience=Config.EARLY_STOPPING_PATIENCE)
-    model_checkpoint = ModelCheckpoint(dirpath=output_dir, filename=Config.FILE_NAME, monitor="val_loss",
+    parser = ArgumentParser()
+    parser.add_argument("--train_directory", type=Path,
+                        default="/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/train",
+                        help="path to the dataset, default: ./dataset")
+    parser.add_argument("--val_directory", type=Path,
+                        default="/home/ai/projects/vehicle-plate-recognition-training/recognition/datasets/val",
+                        help="path to the dataset, default: ./dataset")
+    parser.add_argument("--output_dir", type=Path, default="./output",
+                        help="path to the output directory, default: ./output")
+    parser.add_argument("--epochs", type=int, default=100, help="number of training epochs")
+    parser.add_argument("--device", default="cuda", help="what should be the device for training, default is cuda")
+    parser.add_argument("--mean", nargs="+", type=float, default=[0.4845], help="dataset channel-wise mean")
+    parser.add_argument("--std", nargs="+", type=float, default=[0.1884], help="dataset channel-wise std")
+    parser.add_argument("--alphabets", default='ابپتشثجدزسصطعفقکگلمنوهی+۰۱۲۳۴۵۶۷۸۹',
+                        help="alphabets used in the process")
+    args = parser.parse_args()
+    config = Config()
+    config.update_config_param(args)
+
+    output_dir = mkdir_incremental(config.output_dir)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=config.EARLY_STOPPING_PATIENCE)
+    model_checkpoint = ModelCheckpoint(dirpath=output_dir, filename=config.FILE_NAME, monitor="val_loss",
                                        verbose=True)
     learning_rate_monitor = LearningRateMonitor(logging_interval="epoch")
-    trainer = pl.Trainer(gpus=1 if Config.DEVICE == "cuda" else 0,
-                         max_epochs=Config.EPOCHS,
-                         min_epochs=Config.EPOCHS // 10,
+    trainer = pl.Trainer(gpus=1 if config.device == "cuda" else 0,
+                         max_epochs=config.epochs,
+                         min_epochs=config.epochs // 10,
                          callbacks=[early_stopping, model_checkpoint, learning_rate_monitor],
                          default_root_dir=output_dir)
-    lit_crnn = LitCRNN()
-    train_loader, val_loader = lit_crnn.get_loaders()
+    lit_crnn = LitCRNN(config.IMG_H, config.N_CHANNELS, config.N_CLASSES, config.N_HIDDEN, config.LR,
+                       config.LR_REDUCE_FACTOR, config.LR_PATIENCE)
+    train_loader, val_loader = lit_crnn.get_loaders(config)
     trainer.fit(model=lit_crnn, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
     trainer.test(lit_crnn, ckpt_path="best", dataloaders=val_loader)
     trainer.test(lit_crnn, ckpt_path="best", dataloaders=train_loader)
 
     # Adding artifacts to weights
-    weight_path = join(output_dir, f"{Config.FILE_NAME}.ckpt")
+    weight_path = join(output_dir, f"{config.FILE_NAME}.ckpt")
     best_weight = torch.load(weight_path)
-    best_weight['img_height'] = Config.IMG_H
-    best_weight['img_width'] = Config.IMG_W
-    best_weight['n_channels'] = Config.N_CHANNELS
-    best_weight['n_hidden'] = Config.N_HIDDEN
-    best_weight['mean'] = Config.MEAN
-    best_weight['std'] = Config.STD
-    best_weight['label2char'] = Config.LABEL2CHAR
-    best_weight['n_classes'] = Config.N_CLASSES
+    best_weight['img_height'] = config.IMG_H
+    best_weight['img_width'] = config.IMG_W
+    best_weight['n_channels'] = config.N_CHANNELS
+    best_weight['n_hidden'] = config.N_HIDDEN
+    best_weight['mean'] = config.mean
+    best_weight['std'] = config.std
+    best_weight['label2char'] = config.LABEL2CHAR
+    best_weight['n_classes'] = config.N_CLASSES
     torch.save(best_weight, weight_path)
 
 
